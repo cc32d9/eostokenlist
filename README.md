f9faf969420f6e92c2a74abd59ead04e6d3f24a1
cc32d9 <cc32d9@gmail.com>
2018-06-30 20:38:39 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# EOS Token List Tool
+
+This tool takes an account name as an argument and scans all its
+transactions for token transfers or issuance. It then prints the total
+balance for each token in text or JSON format.
+
+## Installing the software
+
+Packages required for Ubuntu or Debian:
+
+```
+sudo apt-get install -y libjson-xs-perl libjson-perl \
+ libwww-perl liblwp-protocol-https-perl
+
+mkdir -p $HOME/tools
+cd $HOME/tools
+git clone https://github.com/cc32d9/eostokenlist.git
+``` 
+
+CentOS or RHEL can also be used, and you just need to install required
+Perl modules.
+
+## Running the tool
+
+The tool understands several options, and `--acc=ACCOUNTNAME` is
+mandatory. It specifies the account name for which we search for tokens
+and balances.
+
+With `--rpc=URL` you can choose an EOS node where you send your quieries
+to. The URL is listed in `ssl_endpoint` entry of the BP's `/bp.json`
+file.
+
+With `--json`, the tool prints a JSON object with all balances to its
+standard output. Without this option, the output is in plain text.
+
+Also `--verbose` option prints all diagnostics messages. This is used
+mostly for troubleshooting.
+
+```
+# Command-line example
+perl /home/bob/tools/eostokenlist/eostokenlist.pl --acc=ACCOUNTNAME
+```
+
+## Copyright and License
+
+Copyright 2018 cc32d9@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+## Donations
+
+ETH address: `0x7137bfe007B15F05d3BF7819d28419EAFCD6501E`
+
+EOS account: `cc32dninexxx`
+
---
+++ eostokenlist.pl
@@ -0,0 +1,262 @@
+## Tool to display all tokens for an EOS account
+#
+# Copyright 2018 cc32d9@gmail.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+use strict;
+use warnings;
+use Getopt::Long;
+use JSON;
+use LWP::UserAgent;
+use HTTP::Request;
+
+my $account;
+my $rpcurl = 'https://api.cypherglass.com';
+my $verbose;
+my $out_json;
+
+
+{
+    my $ok = GetOptions
+        ('acc=s'        => \$account,
+         'rpc=s'        => \$rpcurl,
+         'json'         => \$out_json,
+         'verbose'      => \$verbose);
+
+
+    if( not $ok or not $account or scalar(@ARGV) > 0 )
+    {
+        error("Usage: $0 --acc=ACCOUNT [OPTIONS]",
+              "Options:",
+              "  --acc=ACCOUNT   EOS account name",
+              "  --rpc=URL       \[$rpcurl\] EOS RPC URL",
+              "  --json          print results in JSON format",
+              "  --verbose       print verbose output");
+        exit 1;
+    }
+}
+
+
+my $ua = LWP::UserAgent->new(keep_alive => 1);
+$ua->timeout(5);
+$ua->env_proxy;
+
+{
+    my $url = $rpcurl . '/v1/chain/get_info';
+    my $resp = $ua->get($rpcurl . '/v1/chain/get_info');
+    if( not $resp->is_success )
+    {
+        error("Cannot access $url: " . $resp->status_line);
+        exit 1;
+    }
+
+    my $content = $resp->decoded_content;
+    my $data = eval { decode_json($content) };
+    if( $@ )
+    {
+        error("Content at $url is not a valid JSON: $content");
+        exit 1;
+    }
+
+    if( not defined($data->{'server_version'}) )
+    {
+        error("$url returned invalid data $content");
+        exit 1;
+    }
+
+    verbose('Retrieved server info',
+            'server_version=' . $data->{'server_version'});
+}
+
+
+sub get_actions
+{
+    my $account = shift;
+    my $pos = shift;
+    my $offset = shift;
+
+    verbose("get_actions acc=$account pos=$pos offset=$offset");
+
+    my $url = $rpcurl . '/v1/history/get_actions';
+
+    my $req = HTTP::Request->new('POST', $url);
+    $req->header('Content-Type' => 'application/json');
+
+    $req->content(encode_json(
+                      {
+                          'account_name' => $account,
+                          'pos' => $pos,
+                          'offset' => $offset,
+                      }));
+
+    my $resp = $ua->request($req);
+    if( not $resp->is_success )
+    {
+        die("Cannot retrieve transactions for account $account");
+    }
+
+    my $content = $resp->decoded_content;
+    my $data = eval { decode_json($content) };
+    if( $@ )
+    {
+        die("Content at $url is not a valid JSON: $content");
+    }
+
+    if( not defined($data->{'actions'}) )
+    {
+        die("RPC result does not contain a list of actions: $content");
+    }
+
+    if( scalar(@{$data->{'actions'}}) == 0 )
+    {
+        die("Empty list of actions for $account");
+    }
+
+    verbose('got ' . scalar(@{$data->{'actions'}}));
+    return $data->{'actions'};
+}
+
+
+sub get_balance
+{
+    my $account = shift;
+    my $code = shift;
+    my $symbol = shift;
+
+    verbose("get_balance acc=$account code=$code symbol=$symbol");
+
+    my $url = $rpcurl . '/v1/chain/get_currency_balance';
+
+    my $req = HTTP::Request->new('POST', $url);
+    $req->header('Content-Type' => 'application/json');
+
+    $req->content(encode_json(
+                      {
+                          'account' => $account,
+                          'code' => $code,
+                          'symbol' => $symbol,
+                      }));
+
+    my $resp = $ua->request($req);
+    if( not $resp->is_success )
+    {
+        die("Cannot retrieve $code balance for account $account");
+    }
+
+    my $content = $resp->decoded_content;
+    my $data = eval { decode_json($content) };
+    if( $@ )
+    {
+        die("Content at $url is not a valid JSON: $content");
+    }
+
+    return $data->[0];
+}
+
+
+
+my @all_actions;
+my $last_action = get_actions($account, -1, -1);
+$last_action = $last_action->[0];
+my $last_seq = $last_action->{'account_action_seq'};
+verbose("Last action srequence: $last_seq");
+
+
+my $seq = 0;
+
+while( $seq <= $last_seq )
+{
+    my $rcount = $last_seq - $seq;
+    $rcount = 50 if $rcount > 50;
+
+    my $acts = get_actions($account, $seq, $rcount);
+    
+    foreach my $action (@{$acts})
+    {
+        push(@all_actions, $action);
+        if( $action->{'account_action_seq'} > $seq )
+        {
+            $seq = $action->{'account_action_seq'};
+        }
+    }
+
+    $seq++;
+}
+
+verbose("Retrieved " . scalar(@all_actions) . " actions for $account");
+
+
+my %contracts;
+
+foreach my $action (@all_actions)
+{
+    my $at = $action->{'action_trace'};
+    my $act = $at->{'act'};
+    if( $act->{'name'} eq 'transfer' or $act->{'name'} eq 'issue' )
+    {
+        my $issuer = $act->{'account'};
+        my $qty = $act->{'data'}{'quantity'};
+        my $currency = $qty;
+        $currency =~ s/^[0-9.]+\s+//;
+        if( not $contracts{$issuer}{$currency} )
+        {
+            verbose("Found token: contract=$issuer currency=$currency");
+            $contracts{$issuer}{$currency} = 1;
+        }
+    }
+}
+
+
+my %balances;
+
+foreach my $issuer (sort keys %contracts)
+{
+    printf("Issuer: %s\n", $issuer) unless $out_json;
+    foreach my $currency (sort keys %{$contracts{$issuer}})
+    {
+        my $balance = get_balance($account, $issuer, $currency);
+        $balance =~ s/\s+\w+$//;
+        $balances{$issuer}{$currency} = $balance;
+        printf("  %s %s\n", $balance, $currency) unless $out_json;
+    }
+}
+
+
+if( $out_json )
+{
+    my $jswriter = JSON->new()->utf8(1)->canonical(1)->pretty(1);
+
+    print $jswriter->encode(\%balances);
+}
+
+exit 0;
+
+
+
+
+
+
+sub error
+{
+    print STDERR (join("\n", @_), "\n");
+}
+
+sub verbose
+{
+    if($verbose)
+    {
+        print STDERR (join("\n", @_), "\n");
+    }
+}
